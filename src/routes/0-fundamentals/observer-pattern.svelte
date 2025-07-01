a394122478539c4d02eb4090a1e41dda7ae1a232
Kris <kris_s2@outlook.com>
2025-07-01 12:10:21 -0700

    kata: subscribing all observers with the subject (Observable)

--- src/routes/0-fundamentals/observer-pattern.svelte
+++ src/routes/0-fundamentals/observer-pattern.svelte
@@ -4,6 +4,15 @@
 
     const subject: Subject = new Subject();
     const observer1: Observer = new Observer();
+    const observer2: Observer = new Observer();
+    const observer3: Observer = new Observer();
+    const observer4: Observer = new Observer();
+
+    subject.subscribe(observer1);
+    subject.subscribe(observer2);
+    subject.subscribe(observer3);
+    subject.subscribe(observer4);
+
 </script>
 
 <section>
