bd31eaeea4f6d3baf3112f2f4cae22dc652d8e96
Kris <kris_s2@outlook.com>
2025-07-01 12:06:37 -0700

    kata: working on Observer pattern in RxJs.

--- src/routes/0-fundamentals/observer-pattern.svelte
+++ src/routes/0-fundamentals/observer-pattern.svelte
@@ -1,5 +1,9 @@
-<script>
-    import Page from "../../lib/Page.svelte";
+<script lang="ts">
+  import { Subject, Observer } from "../../utils/classes";
+  import Page from "../../lib/Page.svelte";
+
+    const subject: Subject = new Subject();
+    const observer1: Observer = new Observer();
 </script>
 
 <section>
