0c36f9aa1383a228b9666ffbf328c5a9f487e637
Kris <kris_s2@outlook.com>
2025-07-20 14:27:44 -0700

    kata: Working on part 1. Adding observer and observable.

--- src/routes/1-building-blocks/observer-notifications.svelte
+++ src/routes/1-building-blocks/observer-notifications.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
     import Page from "$lib/Page.svelte";
+    import { Observable, PartialObserver } from "rxjs";
+
+    const observer: PartialObserver<string> = {
+        next: (value: string) => console.log("Next notification", value)
+    };
+
+    const observable$ = new Observable<string>((subscriber) => {
+        subscriber.next("(sync) Rhinocerous beatle.");
+    });
+
+
+
 </script>
 
 <section>
